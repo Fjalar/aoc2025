bb67c0d7d0e7d2e3fc88e5993c4389a40eecaf1b
Fjalar <fjalarhognason@gmail.com>
2025-12-11 08:38:03 +0100

    Day 11 part 2 I'm a genius edition

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,4 +1,6 @@
+use hashbrown::HashSet;
 use itertools::Itertools;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 advent_of_code::solution!(11);
 
@@ -53,26 +55,141 @@ pub fn part_two(input: &str) -> Option<u64> {
         })
         .collect_vec();
 
-    let start = devices
+    let ans = [("fft", "svr"), ("dac", "fft"), ("out", "dac")]
         .iter()
-        .filter(|device| device.label.eq("svr"))
-        .exactly_one()
-        .unwrap();
+        .map(|&(target, start)| {
+            let mut layers = Vec::new();
 
-    let visited = Vec::<String>::new();
+            layers.push(
+                devices
+                    .iter()
+                    .filter(|device| device.connections.contains(&target.to_owned()))
+                    .cloned()
+                    .collect_vec(),
+            );
 
-    let ans = start.visit_recursively_p2(&visited, &devices, false, false);
+            while !layers
+                .last()
+                .unwrap()
+                .iter()
+                .any(|device| device.label == start)
+            {
+                layers.push(
+                    devices
+                        .iter()
+                        .filter(|device| {
+                            device.connections.iter().any(|label| {
+                                layers
+                                    .last()
+                                    .unwrap()
+                                    .iter()
+                                    .any(|last_layer_device| &last_layer_device.label == label)
+                            })
+                        })
+                        .cloned()
+                        .collect_vec(),
+                );
+            }
+
+            let all_labels = layers
+                .iter()
+                .flat_map(|layer| layer.iter().map(|dev| dev.label.clone()))
+                .collect::<HashSet<_>>();
+
+            layers.iter_mut().flatten().for_each(|dev| {
+                dev.connections
+                    .retain(|label| label == target || all_labels.contains(label))
+            });
+
+            // println!("{layers:?}");
+
+            let pruned_devices = layers.iter().flatten().collect_vec();
+
+            let mut pos_devices = Vec::<PosDevice>::new();
+
+            let mut start_idx: usize = 0;
+            let target_idx: usize = usize::MAX;
+
+            for idx in 0..pruned_devices.len() {
+                let dev = &pruned_devices[idx];
+
+                if dev.label == start {
+                    start_idx = idx;
+                }
+
+                let connections = dev
+                    .connections
+                    .iter()
+                    .map(|label| {
+                        if label == target {
+                            // usize::MAX used as magical number to represent target
+                            usize::MAX
+                        } else {
+                            #[allow(clippy::let_and_return)]
+                            let pos = pruned_devices
+                                .iter()
+                                .position(|find_label| &find_label.label == label)
+                                .unwrap();
+
+                            pos
+                        }
+                    })
+                    .collect_vec();
+                pos_devices.push(PosDevice { connections });
+            }
+
+            // println!("{pos_devices:?}");
+
+            // println!("start: {start_idx}, target: {target_idx}");
+            #[allow(clippy::let_and_return)]
+            let start_to_target = visit_freestanding_p2(&pos_devices, start_idx, target_idx);
+            // println!("{start_to_target}");
+
+            start_to_target
+        })
+        .product::<u64>();
 
     Some(ans)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Device {
     pub label: String,
     pub connections: Vec<String>,
 }
 
-static mut TOTAL_VISITED: u64 = 0;
+#[derive(Debug)]
+struct PosDevice {
+    pub connections: Vec<usize>,
+}
+
+// static mut TOTAL_VISITED: u64 = 0;
+
+fn visit_freestanding_p2(all: &[PosDevice], idx: usize, target: usize) -> u64 {
+    // unsafe {
+    //     TOTAL_VISITED += 1;
+    //     let val = TOTAL_VISITED;
+    //     if val.is_multiple_of(100_000_000) {
+    //         println!("{}", val);
+    //     }
+    // }
+
+    all[idx]
+        .connections
+        .par_iter()
+        .map(|&next_idx| {
+            // print!("{next} ");
+
+            if next_idx == target {
+                1
+            } else if next_idx == usize::MAX {
+                0
+            } else {
+                visit_freestanding_p2(all, next_idx, target)
+            }
+        })
+        .sum::<u64>()
+}
 
 impl Device {
     fn visit_recursively_p1(&self, visited: &[String], all: &[Device]) -> u64 {
@@ -90,53 +207,6 @@ impl Device {
                         .exactly_one()
                         .unwrap()
                         .visit_recursively_p1(&new_visited, all)
-                }
-            })
-            .sum::<u64>()
-    }
-
-    fn visit_recursively_p2(
-        &self,
-        visited: &[String],
-        all: &[Device],
-        visited_fft: bool,
-        visited_dac: bool,
-    ) -> u64 {
-        // println!("{}", self.label);
-        // print!("\t");'
-        unsafe {
-            TOTAL_VISITED += 1;
-            let val = TOTAL_VISITED;
-            println!("{}", val);
-        }
-        let mut new_visited = visited.to_vec();
-        new_visited.push(self.label.clone());
-        self.connections
-            .iter()
-            .map(|next| {
-                // print!("{next} ");
-                if visited.contains(next) {
-                    0
-                } else if next == "out" {
-                    if visited_fft && visited_dac { 1 } else { 0 }
-                } else if next == "fft" && !visited_fft {
-                    all.iter()
-                        .filter(|device| device.label.eq(next))
-                        .exactly_one()
-                        .unwrap()
-                        .visit_recursively_p2(&new_visited, all, true, visited_dac)
-                } else if next == "dac" && !visited_dac {
-                    all.iter()
-                        .filter(|device| device.label.eq(next))
-                        .exactly_one()
-                        .unwrap()
-                        .visit_recursively_p2(&new_visited, all, visited_fft, true)
-                } else {
-                    all.iter()
-                        .filter(|device| device.label.eq(next))
-                        .exactly_one()
-                        .unwrap()
-                        .visit_recursively_p2(&new_visited, all, visited_fft, visited_dac)
                 }
             })
             .sum::<u64>()
